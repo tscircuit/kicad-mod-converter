28ef97b4c6b1a25d36b322b5f477fd68a95e57af
seveibar <seveibar@gmail.com>
2024-05-27 14:15:17 -0700

    wip: pad parsing

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,47 @@
 import parseSExpression from "s-expression"
 
+interface Attributes {
+  at?: [number, number] | [number, number, number]
+  size?: [number, number]
+  layers?: string[]
+  roundrect_rratio?: number
+  uuid?: string
+}
+
+interface Property {
+  key: string
+  val: string
+  attrs: Attributes
+}
+
+interface Pad extends Attributes {
+  name: string
+}
+
+export const formatAttr = (val: any, attrKey: string) => {
+  if (attrKey === "at" || attrKey === "size") {
+    return val.map((n: any) => Number.parseFloat(n.valueOf()))
+  }
+  if (val.length === 2) {
+    return val.valueOf()
+  }
+  if (!Number.isNaN(Number.parseFloat(val))) {
+    return Number.parseFloat(val)
+  }
+  if (Array.isArray(val)) {
+    return val.map((s) => s.valueOf())
+  }
+  return val
+}
+
+export const getAttr = (s: Array<any>, key: string) => {
+  for (const elm of s) {
+    if (Array.isArray(elm) && elm[0] === key) {
+      return formatAttr(elm.slice(1), key)
+    }
+  }
+}
+
 export const parseKicadMod = (fileContent: string) => {
   const kicadSExpr = parseSExpression(fileContent)
 
@@ -14,19 +56,38 @@ export const parseKicadMod = (fileContent: string) => {
       const val = row[2].valueOf()
       const attrs = row.slice(3).reduce((acc: any, attrAr: any[]) => {
         const attrKey = attrAr[0].valueOf()
-        if (attrAr.length === 2) {
-          acc[attrKey] = attrAr[1].valueOf()
-        } else {
-          acc[attrKey] = attrAr.slice(1)
-        }
+        acc[attrKey] = formatAttr(attrAr.slice(1), attrKey)
         return acc
       }, {} as any)
+
       return {
         key,
         val,
         attrs,
-      }
+      } as Property
     })
 
-  console.log(kicadSExpr)
+  const padRows = kicadSExpr.slice(2).filter((row: any[]) => row[0] === "pad")
+
+  const pads: Array<Pad> = []
+
+  for (const row of padRows) {
+    const at = getAttr(row, "at")
+    const size = getAttr(row, "size")
+    const layers = getAttr(row, "layers")
+    const roundrect_rratio = getAttr(row, "roundrect_rratio")
+    const uuid = getAttr(row, "uuid")
+    pads.push({
+      name: row[1].valueOf(),
+      at,
+      size,
+      layers,
+      roundrect_rratio,
+      uuid,
+    })
+  }
+
+  console.log(pads)
+
+  // console.log(kicadSExpr)
 }
