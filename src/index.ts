bbafb82c94fe7a3081f30cbc56b37b5e45258981
seveibar <seveibar@gmail.com>
2024-05-27 12:31:11 -0700

    wip

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,32 @@
 import parseSExpression from "s-expression"
 
 export const parseKicadMod = (fileContent: string) => {
-  const parsed = parseSExpression(fileContent)
-  console.log(parsed)
+  const kicadSExpr = parseSExpression(fileContent)
+
+  // const footp = kicadSExpr[0]
+  const footprintName = kicadSExpr[1].valueOf()
+
+  const properties = kicadSExpr
+    .slice(2)
+    .filter((row: any[]) => row[0] === "property")
+    .map((row: any) => {
+      const key = row[1].valueOf()
+      const val = row[2].valueOf()
+      const attrs = row.slice(3).reduce((acc: any, attrAr: any[]) => {
+        const attrKey = attrAr[0].valueOf()
+        if (attrAr.length === 2) {
+          acc[attrKey] = attrAr[1].valueOf()
+        } else {
+          acc[attrKey] = attrAr.slice(1)
+        }
+        return acc
+      }, {} as any)
+      return {
+        key,
+        val,
+        attrs,
+      }
+    })
+
+  console.log(kicadSExpr)
 }
