4a20bd7d620691f5371db5a77016744ab18e43f1
Slavi Karaslavov <slaviiiiiiiiiii@abv.bg>
2024-06-25 22:04:50 +0300

    finish kicad json serialization

--- src/utils/serialize-s-expression.ts
+++ src/utils/serialize-s-expression.ts
@@ -1,14 +1,14 @@
 export function serializeSExpression(sexp: any[]): string {
-  function serialize(sexp: any): string {
-    if (Array.isArray(sexp)) {
-      return `(${sexp.map(serialize).join(" ")})`
-    } else if (typeof sexp === "string") {
-      // Escape quotes in strings
-      return `"${sexp.replace(/"/g, '\\"')}"`
-    } else {
-      return sexp.toString()
+    function serialize(sexp: any): string {
+        if (Array.isArray(sexp)) {
+            return `\n(${sexp.map(serialize).join(" ")})`;
+        } else if (typeof sexp === "string") {
+            // Escape quotes in strings
+            return `"${sexp.replace(/"/g, '\\"')}"`;
+        } else {
+            return sexp.toString();
+        }
     }
-  }
 
-  return serialize(sexp)
+    return serialize(sexp);
 }
