792b385261df5203baa95665f2eae84a5854aa25
seveibar <seveibar@gmail.com>
2025-01-02 12:05:31 -0800

    first snapshot test

--- tests/fixtures/get-test-fixture.ts
+++ tests/fixtures/get-test-fixture.ts
@@ -1,7 +1,42 @@
-export const getTestFixture = async () => {
+import type { ExecutionContext } from "ava"
+import type { KicadFileName } from "./kicad-file-paths"
+import { readFileSync } from "node:fs"
+import { readdirSync } from "node:fs"
+import { join } from "node:path"
+
+const cache: any = {}
+
+const kicadDir = "kicad-footprints"
+
+export const getTestFixture = () => {
   return {
-    getKicadFile: (fileName: string) => {
-      return Bun.file(`tests/fixtures/${fileName}`).text()
+    getAllKicadFiles() {
+      if (cache.kicadFileList) return cache.kicadFileList as string[]
+
+      // Each kicad file is nested, i.e. kicad-footprints/*.pretty/*.kicad_mod
+      const files = readdirSync(kicadDir, { withFileTypes: true }).flatMap(
+        (dirent) => {
+          if (dirent.isDirectory() && dirent.name.endsWith(".pretty")) {
+            const nestedDir = join(kicadDir, dirent.name)
+            return readdirSync(nestedDir)
+              .filter((file) => file.endsWith(".kicad_mod"))
+              .map((file) => join(dirent.name, file))
+          }
+          return []
+        },
+      )
+      cache.kicadFileList = files
+      return files
+    },
+
+    getKicadFilePath(fname: KicadFileName) {
+      const files = this.getAllKicadFiles()
+      const filePath = files.find((file) => file.endsWith(fname))
+      return join(kicadDir, filePath!)
+    },
+
+    getKicadFile(fname: KicadFileName) {
+      return readFileSync(this.getKicadFilePath(fname), "utf-8")
     },
   }
 }
